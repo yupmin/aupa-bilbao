941762703ecb9f98cf3c0ae5542dc6c7650d97fb
yupmin <yupmin@gmail.com>
2016-08-04 15:52:36 +0900

    commit for chap03

---
+++ src/AppBundle/Admin/ProductAdmin.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+/**
+ * Class ProductAdmin
+ * @package AppBundle\Admin
+ */
+class ProductAdmin extends AbstractAdmin
+{
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('name')
+            ->add('price')
+            ->add('description')
+        ;
+    }
+
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('name')
+            ->add('price')
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('id')
+            ->add('name')
+            ->add('price')
+            ->add('description')
+            ->add('_action', 'actions', [
+                'actions' => [
+                    'edit' => []
+                ]
+            ])
+        ;
+    }
+}
